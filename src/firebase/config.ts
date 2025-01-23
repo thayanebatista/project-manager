e273dd4fccdf44dad019f97de4c7a25b8d0d174d
Thayane Batista <thayane@jera.com.br>
2025-01-23 13:23:55 -0400

    [NEW] firebase keys env

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDlfFAVWujN-Uh5dIIq4JafUyKypmmO9U8',
-  authDomain: 'fisrtfb-f1118.firebaseapp.com',
-  projectId: 'fisrtfb-f1118',
-  storageBucket: 'fisrtfb-f1118.firebasestorage.app',
-  messagingSenderId: '720649823858',
-  appId: '1:720649823858:web:222b4c34d37c90b34244eb',
-  measurementId: 'G-F96CFWTQWC',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
